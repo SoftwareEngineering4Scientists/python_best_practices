07988245585f6fa8db0c3fc7a55f225ec61edb36
jtstanley <jtstan@gmail.com>
2021-10-01 19:08:06 +0000

    Demo for PEP8 standard, showing what NOT to do.

---
+++ not_pep8.py
@@ -0,0 +1,14 @@
+import random
+import os
+import sys
+
+
+def add (   a,    b ):
+    c= a   +b
+    return c
+def rand( a, b  ):
+    rand_val=random.randint(a,b)
+    return rand_val
+
+my_list = ['firstelement', 'secondelement', 'thirdelement', 'fourthelement', 'fifthelement']
+    
